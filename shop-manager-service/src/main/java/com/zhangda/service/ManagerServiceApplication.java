36d6121ace1d85235598c0a1ca3a9eae9d3397ea
zhangda <qzi@qq.com>
2023-03-30 14:51:38 +0800

    service配置mysql、dubbo

---
+++ shop-manager-service/src/main/java/com/zhangda/service/ManagerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.zhangda.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangda
+ * @date: 2023/3/30
+ **/
+@SpringBootApplication
+public class ManagerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerServiceApplication.class, args);
+    }
+}
